c4ba600c10775f6a80e549d678940e43b63c0754
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-11-24 22:26:10 +0300

    Renamed the "show_home" function to "index".

--- places/views.py
+++ places/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def show_home(request):
+def index(request):
     return render(request, 'index.html')
--- project/urls.py
+++ project/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from places.views import show_home
+from places.views import index
 
 urlpatterns = [
-    path('', show_home),
+    path('', index),
     path('admin/', admin.site.urls),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
