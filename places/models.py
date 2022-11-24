a5ba40346f4c5cc0a897f95125fa98d5d8980dec
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-11-24 21:08:53 +0300

    Added Image model.

--- places/models.py
+++ places/models.py
@@ -3,3 +3,12 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(
+        verbose_name='Изображение',
+        blank=True,
+        null=True,
+        upload_to='images',
+    )
