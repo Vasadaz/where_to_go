d1655ade54763150a7b2a8f578efc9d7281a9485
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-11-24 21:29:30 +0300

    Setup media files serving.

--- project/urls.py
+++ project/urls.py
@@ -8,6 +8,5 @@ from places.views import show_home
 urlpatterns = [
     path('', show_home),
     path('admin/', admin.site.urls),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
