bb817beff86d7983041df2deefa1f583f881ee7a
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-11-25 22:32:24 +0300

    Added the "places" view and the "places/<int:id>/" urlpattern.

--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from places.models import Place
 
@@ -27,5 +28,30 @@ def index(request):
 
         geojson_places['features'].append(place_serialize)
 
+    context = {'places': geojson_places}
+    return render(request, 'index.html', context=context)
+
+
+def places(request, id):
+    place = get_object_or_404(Place, id=id)
+    geojson_places = {
+        'type': 'FeatureCollection',
+        'features': [],
+    }
+
+    place_serialize = {
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [place.latitude, place.longitude],
+        },
+        'properties': {
+            'title': place.title,
+            'placeId': place.place_id,
+            'detailsUrl': place.details_url,
+        }
+    }
+
+    geojson_places['features'].append(place_serialize)
     context = {'places': geojson_places}
     return render(request, 'index.html', context=context)
--- project/urls.py
+++ project/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from places.views import index
+from places.views import index, places
 
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
+    path('places/<int:id>/', places),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
