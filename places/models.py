15c4c5bf17fc8973e7af68fce1fbad812f0ecbb1
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-11-25 21:59:44 +0300

    Updated the Place model.

--- places/models.py
+++ places/models.py
@@ -3,6 +3,18 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
+    place_id = models.CharField(max_length=50, blank=True)
+    latitude = models.FloatField(default=0.0)  # Широта
+    longitude = models.FloatField(default=0.0)  # Долгота
+    details_url = models.CharField(max_length=50, default='path')
+
+    class Meta:
+        verbose_name = 'place'
+        verbose_name_plural = 'places'
+
+    def __str__(self):
+        return f'{self.title})'
+
 
 class Image(models.Model):
     title = models.CharField(max_length=200)
