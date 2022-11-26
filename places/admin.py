0426bc45e4faac561a3023b457446557bf48b3cf
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-11-26 21:46:32 +0300

    Added inlines for PlaceAdmin.

--- places/admin.py
+++ places/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
     list_display = (
         'title',
     )
