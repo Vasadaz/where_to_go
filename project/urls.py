ac9f82101193625092cf3b87521e7f9c1edf2967
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-11-27 12:50:18 +0300

    Installed django-admin-sortable2==2.1.4.

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from places.views import index, places
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
     path('places/<int:place_id>/', places, name='places'),
+    path('tinymce/', include('tinymce.urls')),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
