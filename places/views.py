cd8a17d134bc1682aebd7ba7769ef1da25afb8f5
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-11-27 12:54:16 +0300

    Updated views.py

--- places/views.py
+++ places/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 
-from places.models import Image, Place
+from places.models import Place
 
 
 def index(request):
